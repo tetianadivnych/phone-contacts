39a681c0c872a22567cc19176ab151da1407bfe7
tetianadivnych <113429084+tetianadivnych@users.noreply.github.com>
2023-07-04 21:03:38 +0300

    init project

---
+++ src/test/java/com/divnych/phonecontacts/PhoneContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divnych.phonecontacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
